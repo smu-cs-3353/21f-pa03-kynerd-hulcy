616841088e5257ee1535e9ee0ad664e5b1527153
j-kynerd <jkynerd@smu.edu>
2021-11-03 13:21:56 -0500

    Initial push / CMakeLists setup for Boost

---
+++ cmake-build-debug/CMakeFiles/21f_pa03_kynerd_hulcy.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "21f_pa03_kynerd_hulcy"
+  "21f_pa03_kynerd_hulcy.pdb"
+  "CMakeFiles/21f_pa03_kynerd_hulcy.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/21f_pa03_kynerd_hulcy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/21f_pa03_kynerd_hulcy.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 21f_pa03_kynerd_hulcy.
+# This may be replaced when dependencies are built.
