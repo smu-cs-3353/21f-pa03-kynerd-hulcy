09cd7d678e3bc7c345abaf9bc8dfc1e7c038b8a9
maddiehulcy <60118685+maddiehulcy@users.noreply.github.com>
2021-11-09 16:01:00 -0600

    before switching node and edge

--- girvan_newman.h
+++ girvan_newman.h
@@ -66,9 +66,6 @@ namespace algos {
         }
 
         while (current_vertex != end_of_graph) {
-            if(*current_vertex == 1) {
-                std::cout << "current node: "<< *current_vertex << std:: endl;
-            }
             std::queue<v_iterator> v_queue;
             std::stack<v_iterator> v_stack;
 
@@ -92,7 +89,6 @@ namespace algos {
                 auto vertex2 = vs.first;
                 while (vertex2 != end_of_graph) {
                     if (boost::edge(*vertex1, *vertex2, g).second) {
-                        std::cout << *vertex1 << ", " << *vertex2 << std:: endl;
                         if (vertex_properties.at(vertex2).distance == -1) {
                             v_queue.push(vertex2);
                             vertex_properties.at(vertex2).distance = vertex_properties.at(vertex1).distance + 1;
@@ -122,7 +118,7 @@ namespace algos {
                         e_descriptor edge_desc = boost::edge(*predecessor, *vertex2, g).first;
                         double tempC = centrality_map.at(edge_desc);
                         centrality_map.erase(edge_desc);
-                        centrality_map.insert(std::pair<e_descriptor, double>(edge_desc, tempC + vertex_properties.at(predecessor).dependency));
+                        centrality_map.insert(std::pair<e_descriptor, double>(edge_desc, tempC + vertex_properties.at(predecessor).dependency/2));
                     }
 //                    if (vertex2 != current_vertex) {
 //                        centrality_map.at(vertex2) += vertex_properties.at(vertex2).dependency/2;
@@ -141,13 +137,13 @@ namespace algos {
         boost::shared_array_property_map<double, boost::property_map<graph, boost::vertex_index_t>::const_type>
                 centrality_map(num_vertices(g), get(boost::vertex_index, g));
         boost::brandes_betweenness_centrality(g, centrality_map);
-
-        std::cout << "Correct:" << std::endl;
-        std::cout << "0: " << centrality_map[0] << std::endl;
-        std::cout << "1: " << centrality_map[1] << std::endl;
-        std::cout << "2: " << centrality_map[2] << std::endl;
-        std::cout << "3: " << centrality_map[3] << std::endl;
-        std::cout << "4: " << centrality_map[4] << std::endl;
+//
+//        std::cout << "Correct:" << std::endl;
+//        std::cout << "0: " << centrality_map[0] << std::endl;
+//        std::cout << "1: " << centrality_map[1] << std::endl;
+//        std::cout << "2: " << centrality_map[2] << std::endl;
+//        std::cout << "3: " << centrality_map[3] << std::endl;
+//        std::cout << "4: " << centrality_map[4] << std::endl;
 //        std::cout << "5: " << centrality_map[5] << std::endl;
 //        std::cout << "6: " << centrality_map[6] << std::endl;
 //        std::cout << "7: " << centrality_map[7] << std::endl;
@@ -155,7 +151,23 @@ namespace algos {
 //        std::cout << "9: " << centrality_map[9] << std::endl;
 //        std::cout << "10: " << centrality_map[10] << std::endl;
 //        std::cout << "11: " << centrality_map[11] << std::endl;
-//        std::cout << "12: " << centrality_map[12] << std::endl << std::endl;
+//        //std::cout << "12: " << centrality_map[12] << std::endl << std::endl;
+
+        std::map<e_descriptor, double> edge_centralities;
+        auto ecm = boost::make_assoc_property_map(edge_centralities);
+        boost::brandes_betweenness_centrality(g, boost::edge_centrality_map(ecm));
+
+        std::cout << "done c" << std::endl;
+
+        for (auto it = edge_centralities.begin(); it != edge_centralities.end(); it++)
+        {
+            std::cout << it->first    // string (key)
+                      << ':'
+                      << it->second   // string's value
+                      << std::endl;
+        }
+
+
 
 
         std::cout << "Ours:" << std::endl;
